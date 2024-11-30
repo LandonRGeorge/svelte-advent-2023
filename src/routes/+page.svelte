24e381cdb02383160e976ac204223668e183c33c
LandonRGeorge <landon.robert.george@gmail.com>
2024-11-30 06:54:22 -0600

    add day2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <h1>Home</h1>
 <ul>
     <li><a href="/day1">Day 1 - Naughty or Nice</a></li>
+    <li><a href="/day2">Day 2 - Merry Munch-o-Meter</a></li>
 </ul>
 
 <style>
---
+++ src/routes/day2/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    let tally = $state(0);
+</script>
+
+<h1>Day 2 - Merry Munch-o-Meter</h1>
+<div>
+    <button onclick={() => tally++}>Increment</button>
+    <button onclick={() => tally--}>Decrement</button>
+    <button onclick={() => (tally = 0)}>Reset</button>
+</div>
+<div>{tally} {tally === 1 ? "Cookie" : "Cookies"}</div>
